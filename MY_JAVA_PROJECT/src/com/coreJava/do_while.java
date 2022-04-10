7e149ec2bcf330f2104711943d1512b28980f6d4
RabiyaAnjum <92446021+RabiyaAnjum@users.noreply.github.com>
2022-04-10 20:30:52 +0530

    Add files via upload

---
+++ MY_JAVA_PROJECT/src/com/coreJava/do_while.java
@@ -0,0 +1,16 @@
+package com.tns.core.java;
+
+public class do_while {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0;
+		do {
+			x++;
+			System.out.println(x);
+			
+		}while(++x<10);
+
+	}
+
+}
