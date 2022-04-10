dcd9bff298dee3486089a4e61e4d63f0007cca4f
RabiyaAnjum <92446021+RabiyaAnjum@users.noreply.github.com>
2022-04-10 20:23:50 +0530

    Add files via upload

---
+++ MY_JAVA_PROJECT/src/com/coreJava/Prime_Number.java
@@ -0,0 +1,15 @@
+package com.tns.core.java;
+import java.util.Scanner;
+
+public class Prime_Number {
+	public static void main(java.lang.String[] args) {
+		 System.out.println("Enter A Number");
+int num = new Scanner(System.in).nextInt();
+if (num % 2 == 0) {
+System.out.println("not prime");
+} else {
+System.out.println("Prime");
+}
+}
+}
+
