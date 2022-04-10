7a7a5f8bb3d6a1ff5255f7229fb3ca8e7b3a8e03
RabiyaAnjum <92446021+RabiyaAnjum@users.noreply.github.com>
2022-04-10 20:17:14 +0530

    Add files via upload
    left_tringle_star_pattern

---
+++ MY_JAVA_PROJECT/src/com/coreJava/Left_Triangle_Star_Pattern.java
@@ -0,0 +1,23 @@
+package com.tns.core.java;
+
+public class Left_Triangle_Star_Pattern {
+
+	public static void main(java.lang.String[] args) {
+
+int n=7;  
+//inner loop  
+for (int i= 0; i<= n; i++)  
+{  
+//outer loop  
+for (int j=1; j<=n-i; j++)  
+{  
+System.out.print(" ");  
+}  
+for (int k=0;k<=i;k++)  
+{  
+System.out.print("*");  
+}   
+System.out.println("");  
+}  
+}  
+}  
