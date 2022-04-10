0e8ab3c5a40e12b99edbcbba58c48a56db770287
RabiyaAnjum <92446021+RabiyaAnjum@users.noreply.github.com>
2022-04-10 20:31:30 +0530

    Add files via upload

---
+++ MY_JAVA_PROJECT/src/com/coreJava/Forloop.java
@@ -0,0 +1,16 @@
+package com.tns.core.java;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub 
+		int x=0;
+		for(System.out.println("This is new");x<3;x++)
+		{
+			System.out.println("This is old");
+		}
+
+	}
+
+}
